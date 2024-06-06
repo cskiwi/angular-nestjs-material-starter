280664b95b6c0e17e7cbb754d32147e3137ff206
Glenn Latomme <glenn.latomme@arcelormittal.com>
2024-06-06 11:53:18 +0200

    adding default route

--- libs/frontend/root/src/config/app.routes.ts
+++ libs/frontend/root/src/config/app.routes.ts
@@ -1,5 +1,9 @@
 import { Route } from '@angular/router';
+import { pageHomeRoutes } from '@app/frontend-pages-home';
 
 export const appRoutes: Route[] = [
- 
+  {
+    path: '',
+    children: pageHomeRoutes,
+  },
 ];
